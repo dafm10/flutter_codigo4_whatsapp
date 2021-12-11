fc26f1ba3ed5564cd1deb3457e7973af6263f906
dafm10 <dafm.10@gmail.com>
2021-12-10 21:13:13 -0500

    List

--- lib/data/data_dummy.dart
+++ lib/data/data_dummy.dart
@@ -1,8 +1,33 @@
+import 'package:flutter_codigo4_whatsapp/models/chat_message.dart';
 import 'package:flutter_codigo4_whatsapp/models/chat_model.dart';
+
+List<ChatMessage> messages = [
+  ChatMessage(
+    messageContent: "Hola, cómo estas?",
+    messageType: "me",
+  ),
+  ChatMessage(
+    messageContent: "Estoy bien, gracias, tú?",
+    messageType: "other",
+  ),
+  ChatMessage(
+    messageContent: "Todo muy bien, qué haces?",
+    messageType: "me",
+  ),
+  ChatMessage(
+    messageContent: "Puedes ayudarme con algo?",
+    messageType: "me",
+  ),
+  ChatMessage(
+    messageContent: "Si claro, dime",
+    messageType: "other",
+  ),
+];
 
 List<ChatModel> dataListChat = [
   ChatModel(
-    avatarUrl: "https://static.wikia.nocookie.net/esstarwars/images/e/eb/ArtooTFA2-Fathead.png/revision/latest?cb=20150926172435",
+    avatarUrl:
+        "https://static.wikia.nocookie.net/esstarwars/images/e/eb/ArtooTFA2-Fathead.png/revision/latest?cb=20150926172435",
     nameUser: "R2D2",
     message: "Ya he enviado el encargo",
     countMessage: 0,
@@ -10,7 +35,8 @@ List<ChatModel> dataListChat = [
     isWriting: false,
   ),
   ChatModel(
-    avatarUrl: "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    avatarUrl:
+        "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     nameUser: "Juan Carlos Torres",
     message: "Acabo de llegar a la casa, abreme la puerta",
     countMessage: 3,
@@ -18,7 +44,8 @@ List<ChatModel> dataListChat = [
     isWriting: false,
   ),
   ChatModel(
-    avatarUrl: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    avatarUrl:
+        "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     nameUser: "Karla Gonzles",
     message: "Flutter es genial!!!",
     countMessage: 0,
@@ -26,7 +53,8 @@ List<ChatModel> dataListChat = [
     isWriting: true,
   ),
   ChatModel(
-    avatarUrl: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    avatarUrl:
+        "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     nameUser: "Ximena Lopez",
     message: "Ya he enviado el encargo",
     countMessage: 1,
@@ -34,7 +62,8 @@ List<ChatModel> dataListChat = [
     isWriting: false,
   ),
   ChatModel(
-    avatarUrl: "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    avatarUrl:
+        "https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     nameUser: "Juan Carlos Torres",
     message: "Acabo de llegar a la casa, abreme la puerta",
     countMessage: 3,
@@ -42,7 +71,8 @@ List<ChatModel> dataListChat = [
     isWriting: false,
   ),
   ChatModel(
-    avatarUrl: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    avatarUrl:
+        "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
     nameUser: "Karla Gonzles",
     message: "Flutter es genial!!!",
     countMessage: 0,
